0bf96f2099af754dba7609e39496258175fb4b68
David K <davek032@outlook.com>
2024-02-01 17:33:06 +0000

    #25 Servo support and config file added

--- skullOS.Modules/Prop.cs
+++ skullOS.Modules/Prop.cs
@@ -1,5 +1,8 @@
-﻿using skullOS.HardwareServices;
+﻿using Iot.Device.ServoMotor;
+using skullOS.Core;
+using skullOS.HardwareServices;
 using skullOS.HardwareServices.Interfaces;
+using System.Device.Pwm.Drivers;
 using Timer = System.Timers.Timer;
 
 namespace skullOS.Modules
@@ -11,33 +14,52 @@ namespace skullOS.Modules
 
         static Timer PlayIdleSound;
         double interval = 30000;
-
         string[] sounds;
         int numberOfIdles;
 
+        ServoMotor leftFlap;
+        ServoMotor rightFlap;
+
+        Dictionary<string, string> propSettings;
         public Prop()
         {
-            SpeakerService = new SpeakerService();
-            SpeakerService.PlayAudio(@"Resources/computer-startup-music.mp3"); //This one won't await :(
-            sounds = Directory.GetFiles(@"Resources/Haro/Idles");
-            numberOfIdles = sounds.Length;
+            propSettings = SettingsLoader.LoadConfig(@"Data/PropSettings.txt");
 
-            PlayIdleSound = new Timer(interval);
-            PlayIdleSound.AutoReset = true;
-            PlayIdleSound.Elapsed += PlayIdleSound_Elapsed;
-            PlayIdleSound.Start();
+            if (propSettings.ContainsKey("Sounds"))
+            {
+                SpeakerService = new SpeakerService();
+                SpeakerService.PlayAudio(@"Resources/computer-startup-music.mp3"); //This one won't await :(
+                sounds = Directory.GetFiles(@"Resources/Haro/Idles");
+                numberOfIdles = sounds.Length;
 
-            //Left and right eye, these are next to each other so it should be easy to tell
-            Dictionary<string, int> pins = new Dictionary<string, int>
+                PlayIdleSound = new Timer(interval);
+                PlayIdleSound.AutoReset = true;
+                PlayIdleSound.Elapsed += PlayIdleSound_Elapsed;
+                PlayIdleSound.Start();
+            }
+            if (propSettings.ContainsKey("Lights"))
+            {
+                //Left and right eye, these are next to each other so it should be easy to tell
+                Dictionary<string, int> pins = new Dictionary<string, int>
                     {
                         { "LeftEye", 22 },
                         {"RightEye", 23 }
                     };
-            LedService = new LedService(pins);
-            foreach (var item in LedService.GetLeds())
+                LedService = new LedService(pins);
+                foreach (var item in LedService.GetLeds())
+                {
+                    int pin = item.Value;
+                    LedService.TurnOn(pin);
+                }
+            }
+            if (propSettings.ContainsKey("Servos"))
             {
-                int pin = item.Value;
-                LedService.TurnOn(pin);
+                var leftPWM = new SoftwarePwmChannel(5, 50);
+                leftFlap = new ServoMotor(leftPWM);
+                leftFlap.Start();
+                var rightPWM = new SoftwarePwmChannel(6, 50);
+                rightFlap = new ServoMotor(rightPWM);
+                rightFlap.Start();
             }
         }
 
@@ -46,6 +68,34 @@ namespace skullOS.Modules
             var random = new Random();
             int selection = random.Next(0, numberOfIdles + 1);
             SpeakerService.PlayAudio(sounds[selection]);
+            if (propSettings.ContainsKey("Servos"))
+            {
+                if (random.NextSingle() <= 0.5)
+                {
+                    FlapEar(true);
+                }
+                else
+                {
+                    FlapEar(false);
+                }
+            }
+        }
+
+        private async void FlapEar(bool useLeft = true)
+        {
+            if (useLeft)
+            {
+                leftFlap.WritePulseWidth(1500);
+                await Task.Delay(1500);
+                leftFlap.WritePulseWidth(0);
+            }
+            else
+            {
+                rightFlap.WritePulseWidth(1500);
+                await Task.Delay(1500);
+                rightFlap.WritePulseWidth(0);
+            }
+
         }
 
         public override void OnAction(object? sender, EventArgs e)
