4433df3ee8c96edba5ad3e04a01da65550bf8b43
David K <davek032@outlook.com>
2024-02-01 16:48:00 +0000

    #25 Idle Sounds added

--- skullOS.Modules/Prop.cs
+++ skullOS.Modules/Prop.cs
@@ -1,5 +1,6 @@
 ﻿using skullOS.HardwareServices;
 using skullOS.HardwareServices.Interfaces;
+using Timer = System.Timers.Timer;
 
 namespace skullOS.Modules
 {
@@ -7,10 +8,31 @@ namespace skullOS.Modules
     {
         public ISpeakerService SpeakerService { get; set; }
 
+        static Timer PlayIdleSound;
+        double interval = 30000;
+
+        string[] sounds;
+        int numberOfIdles;
+
         public Prop()
         {
             SpeakerService = new SpeakerService();
             SpeakerService.PlayAudio(@"Resources/computer-startup-music.mp3"); //This one won't await :(
+
+            sounds = Directory.GetFiles(@"Resources/Haro/Idles");
+            numberOfIdles = sounds.Length;
+
+            PlayIdleSound = new Timer(interval);
+            PlayIdleSound.AutoReset = true;
+            PlayIdleSound.Elapsed += PlayIdleSound_Elapsed;
+            PlayIdleSound.Start();
+        }
+
+        private void PlayIdleSound_Elapsed(object? sender, System.Timers.ElapsedEventArgs e)
+        {
+            var random = new Random();
+            int selection = random.Next(0, numberOfIdles + 1);
+            SpeakerService.PlayAudio(sounds[selection]);
         }
 
         public override void OnAction(object? sender, EventArgs e)
