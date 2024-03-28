d485ceb16b959b7bd3e97468cd145be69a8222f5
David K <davek032@outlook.com>
2024-03-28 12:00:02 +0000

    #47 Prop test? Prop test!

---
+++ ModuleTests/PropTest.cs
@@ -0,0 +1,27 @@
+﻿using Moq;
+using skullOS.HardwareServices.Interfaces;
+using skullOS.Modules;
+
+namespace ModuleTests
+{
+    public class PropTest
+    {
+        Prop sut;
+        string pathToTestData = @"TestData/PropSettings.txt";
+
+        public PropTest()
+        {
+            Mock<ISpeakerService> speakerMock = new();
+            speakerMock.Setup(speaker => speaker.PlayAudio(It.IsAny<string>())).Returns(Task.CompletedTask);
+            Mock<ILedService> ledMock = new();
+
+            sut = new Prop(speakerMock.Object, ledMock.Object, pathToTestData);
+        }
+
+        [Fact]
+        public void CanCreateProp()
+        {
+            Assert.NotNull(sut);
+        }
+    }
+}
