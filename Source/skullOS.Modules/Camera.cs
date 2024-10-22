415a55a11347b2a9af80a93d025c9e81a35fac97
David K <davek032@outlook.com>
2024-10-22 20:49:59 +0100

    More fixes
    Validated core functionality is now working - still need to deal with the awful video resolution

--- Source/skullOS.Modules/Camera.cs
+++ Source/skullOS.Modules/Camera.cs
@@ -97,12 +97,12 @@ namespace skullOS.Modules
                         //This should be reading from the file!
                         if (buzService == null)
                         {
-                            BuzzerService = new BuzzerService(12);
+                            BuzzerService = new BuzzerService(13);
                         }
                         else
                         {
                             BuzzerService = buzService;
-                            BuzzerService.SetBuzzer(12);
+                            BuzzerService.SetBuzzer(13);
                         }
                         useBuzzer = true;
                     }
